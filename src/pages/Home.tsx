181fa20466ef95e30f443be56d84db43b2882153
Deivis <deivisutp@gmail.com>
2021-06-24 22:50:46 -0300

    third class

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,11 +8,14 @@ import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
 import { useAuth } from '../hooks/useAuth';
+import { FormEvent } from 'react';
+import { useState } from 'react';
+import { database } from '../services/firebase';
 
 export function Home() {
     const { user, signWithGoogle } = useAuth();
-
     const history = useHistory();
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreteRoom() {
         if (!user) {
@@ -21,11 +24,28 @@ export function Home() {
         history.push('rooms/new');
     }
 
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert("Room does not exists");
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
+    }
+
     return (
         <div id="page-auth">
             <aside>
                 <img src={illust} alt="Ilustração perguntas e respostas" />
-                <strong>Crie salas de Q&amo;A ao vivo</strong>
+                <strong>Crie salas de Q&amo; A ao vivo</strong>
                 <p>Tire as dúvidas de sua audiência em tempo real</p>
             </aside>
             <main className="main-content">
@@ -40,6 +60,8 @@ export function Home() {
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">Entrar na sala</Button>
                     </form>
